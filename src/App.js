d3b927f5043815f32ef6208dc71870187103e8e7
Alkin Maystorov <alkin971@gmail.com>
2022-03-09 18:23:49 +0100

    Setup basic navbar with animations.

--- src/App.js
+++ src/App.js
@@ -10,6 +10,194 @@ const App = () => {
   return (
     <>
       <Navbar />
+      <main>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe omnis
+          neque sed perferendis dolores eos animi accusantium iusto! Qui facilis
+          maiores dolorem cumque dolore consequatur, veniam nobis, magnam, dolor
+          voluptatum doloremque cupiditate. Molestias explicabo, quisquam ea
+          labore porro ipsa placeat minus nostrum. Provident, saepe libero
+          dolorum modi perferendis officiis. Quibusdam commodi quis similique
+          est. Laudantium placeat, distinctio, provident debitis explicabo
+          voluptas dolores omnis soluta minus culpa, quae autem nihil! In
+          repellat velit molestiae, qui a facere odit voluptatem aliquid aut
+          officia numquam voluptates. Numquam impedit expedita nemo corporis
+          excepturi, ut officiis dolorem velit aspernatur voluptate delectus
+          iste facilis at ratione. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Saepe omnis neque sed perferendis dolores eos animi
+          accusantium iusto! Qui facilis maiores dolorem cumque dolore
+          consequatur, veniam nobis, magnam, dolor voluptatum doloremque
+          cupiditate. Molestias explicabo, quisquam ea labore porro ipsa placeat
+          minus nostrum. Provident, saepe libero dolorum modi perferendis
+          officiis. Quibusdam commodi quis similique est. Laudantium placeat,
+          distinctio, provident debitis explicabo voluptas dolores omnis soluta
+          minus culpa, quae autem nihil! In repellat velit molestiae, qui a
+          facere odit voluptatem aliquid aut officia numquam voluptates. Numquam
+          impedit expedita nemo corporis excepturi, ut officiis dolorem velit
+          aspernatur voluptate delectus iste facilis at ratione. Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Saepe omnis neque sed
+          perferendis dolores eos animi accusantium iusto! Qui facilis maiores
+          dolorem cumque dolore consequatur, veniam nobis, magnam, dolor
+          voluptatum doloremque cupiditate. Molestias explicabo, quisquam ea
+          labore porro ipsa placeat minus nostrum. Provident, saepe libero
+          dolorum modi perferendis officiis. Quibusdam commodi quis similique
+          est. Laudantium placeat, distinctio, provident debitis explicabo
+          voluptas dolores omnis soluta minus culpa, quae autem nihil! In
+          repellat velit molestiae, qui a facere odit voluptatem aliquid aut
+          officia numquam voluptates. Numquam impedit expedita nemo corporis
+          excepturi, ut officiis dolorem velit aspernatur voluptate delectus
+          iste facilis at ratione. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Saepe omnis neque sed perferendis dolores eos animi
+          accusantium iusto! Qui facilis maiores dolorem cumque dolore
+          consequatur, veniam nobis, magnam, dolor voluptatum doloremque
+          cupiditate. Molestias explicabo, quisquam ea labore porro ipsa placeat
+          minus nostrum. Provident, saepe libero dolorum modi perferendis
+          officiis. Quibusdam commodi quis similique est. Laudantium placeat,
+          distinctio, provident debitis explicabo voluptas dolores omnis soluta
+          minus culpa, quae autem nihil! In repellat velit molestiae, qui a
+          facere odit voluptatem aliquid aut officia numquam voluptates. Numquam
+          impedit expedita nemo corporis excepturi, ut officiis dolorem velit
+          aspernatur voluptate delectus iste facilis at ratione. Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Saepe omnis neque sed
+          perferendis dolores eos animi accusantium iusto! Qui facilis maiores
+          dolorem cumque dolore consequatur, veniam nobis, magnam, dolor
+          voluptatum doloremque cupiditate. Molestias explicabo, quisquam ea
+          labore porro ipsa placeat minus nostrum. Provident, saepe libero
+          dolorum modi perferendis officiis. Quibusdam commodi quis similique
+          est. Laudantium placeat, distinctio, provident debitis explicabo
+          voluptas dolores omnis soluta minus culpa, quae autem nihil! In
+          repellat velit molestiae, qui a facere odit voluptatem aliquid aut
+          officia numquam voluptates. Numquam impedit expedita nemo corporis
+          excepturi, ut officiis dolorem velit aspernatur voluptate delectus
+          iste facilis at ratione. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Saepe omnis neque sed perferendis dolores eos animi
+          accusantium iusto! Qui facilis maiores dolorem cumque dolore
+          consequatur, veniam nobis, magnam, dolor voluptatum doloremque
+          cupiditate. Molestias explicabo, quisquam ea labore porro ipsa placeat
+          minus nostrum. Provident, saepe libero dolorum modi perferendis
+          officiis. Quibusdam commodi quis similique est. Laudantium placeat,
+          distinctio, provident debitis explicabo voluptas dolores omnis soluta
+          minus culpa, quae autem nihil! In repellat velit molestiae, qui a
+          facere odit voluptatem aliquid aut officia numquam voluptates. Numquam
+          impedit expedita nemo corporis excepturi, ut officiis dolorem velit
+          aspernatur voluptate delectus iste facilis at ratione. Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Saepe omnis neque sed
+          perferendis dolores eos animi accusantium iusto! Qui facilis maiores
+          dolorem cumque dolore consequatur, veniam nobis, magnam, dolor
+          voluptatum doloremque cupiditate. Molestias explicabo, quisquam ea
+          labore porro ipsa placeat minus nostrum. Provident, saepe libero
+          dolorum modi perferendis officiis. Quibusdam commodi quis similique
+          est. Laudantium placeat, distinctio, provident debitis explicabo
+          voluptas dolores omnis soluta minus culpa, quae autem nihil! In
+          repellat velit molestiae, qui a facere odit voluptatem aliquid aut
+          officia numquam voluptates. Numquam impedit expedita nemo corporis
+          excepturi, ut officiis dolorem velit aspernatur voluptate delectus
+          iste facilis at ratione. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Saepe omnis neque sed perferendis dolores eos animi
+          accusantium iusto! Qui facilis maiores dolorem cumque dolore
+          consequatur, veniam nobis, magnam, dolor voluptatum doloremque
+          cupiditate. Molestias explicabo, quisquam ea labore porro ipsa placeat
+          minus nostrum. Provident, saepe libero dolorum modi perferendis
+          officiis. Quibusdam commodi quis similique est. Laudantium placeat,
+          distinctio, provident debitis explicabo voluptas dolores omnis soluta
+          minus culpa, quae autem nihil! In repellat velit molestiae, qui a
+          facere odit voluptatem aliquid aut officia numquam voluptates. Numquam
+          impedit expedita nemo corporis excepturi, ut officiis dolorem velit
+          aspernatur voluptate delectus iste facilis at ratione. Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Saepe omnis neque sed
+          perferendis dolores eos animi accusantium iusto! Qui facilis maiores
+          dolorem cumque dolore consequatur, veniam nobis, magnam, dolor
+          voluptatum doloremque cupiditate. Molestias explicabo, quisquam ea
+          labore porro ipsa placeat minus nostrum. Provident, saepe libero
+          dolorum modi perferendis officiis. Quibusdam commodi quis similique
+          est. Laudantium placeat, distinctio, provident debitis explicabo
+          voluptas dolores omnis soluta minus culpa, quae autem nihil! In
+          repellat velit molestiae, qui a facere odit voluptatem aliquid aut
+          officia numquam voluptates. Numquam impedit expedita nemo corporis
+          excepturi, ut officiis dolorem velit aspernatur voluptate delectus
+          iste facilis at ratione. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Saepe omnis neque sed perferendis dolores eos animi
+          accusantium iusto! Qui facilis maiores dolorem cumque dolore
+          consequatur, veniam nobis, magnam, dolor voluptatum doloremque
+          cupiditate. Molestias explicabo, quisquam ea labore porro ipsa placeat
+          minus nostrum. Provident, saepe libero dolorum modi perferendis
+          officiis. Quibusdam commodi quis similique est. Laudantium placeat,
+          distinctio, provident debitis explicabo voluptas dolores omnis soluta
+          minus culpa, quae autem nihil! In repellat velit molestiae, qui a
+          facere odit voluptatem aliquid aut officia numquam voluptates. Numquam
+          impedit expedita nemo corporis excepturi, ut officiis dolorem velit
+          aspernatur voluptate delectus iste facilis at ratione. Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Saepe omnis neque sed
+          perferendis dolores eos animi accusantium iusto! Qui facilis maiores
+          dolorem cumque dolore consequatur, veniam nobis, magnam, dolor
+          voluptatum doloremque cupiditate. Molestias explicabo, quisquam ea
+          labore porro ipsa placeat minus nostrum. Provident, saepe libero
+          dolorum modi perferendis officiis. Quibusdam commodi quis similique
+          est. Laudantium placeat, distinctio, provident debitis explicabo
+          voluptas dolores omnis soluta minus culpa, quae autem nihil! In
+          repellat velit molestiae, qui a facere odit voluptatem aliquid aut
+          officia numquam voluptates. Numquam impedit expedita nemo corporis
+          excepturi, ut officiis dolorem velit aspernatur voluptate delectus
+          iste facilis at ratione. labore porro ipsa placeat minus nostrum.
+          Provident, saepe libero dolorum modi perferendis officiis. Quibusdam
+          commodi quis similique est. Laudantium placeat, distinctio, provident
+          debitis explicabo voluptas dolores omnis soluta minus culpa, quae
+          autem nihil! In repellat velit molestiae, qui a facere odit voluptatem
+          aliquid aut officia numquam voluptates. Numquam impedit expedita nemo
+          corporis excepturi, ut officiis dolorem velit aspernatur voluptate
+          delectus iste facilis at ratione. labore porro ipsa placeat minus
+          nostrum. Provident, saepe libero dolorum modi perferendis officiis.
+          Quibusdam commodi quis similique est. Laudantium placeat, distinctio,
+          provident debitis explicabo voluptas dolores omnis soluta minus culpa,
+          quae autem nihil! In repellat velit molestiae, qui a facere odit
+          voluptatem aliquid aut officia numquam voluptates. Numquam impedit
+          expedita nemo corporis excepturi, ut officiis dolorem velit aspernatur
+          voluptate delectus iste facilis at ratione. labore porro ipsa placeat
+          minus nostrum. Provident, saepe libero dolorum modi perferendis
+          officiis. Quibusdam commodi quis similique est. Laudantium placeat,
+          distinctio, provident debitis explicabo voluptas dolores omnis soluta
+          minus culpa, quae autem nihil! In repellat velit molestiae, qui a
+          facere odit voluptatem aliquid aut officia numquam voluptates. Numquam
+          impedit expedita nemo corporis excepturi, ut officiis dolorem velit
+          aspernatur voluptate delectus iste facilis at ratione. labore porro
+          ipsa placeat minus nostrum. Provident, saepe libero dolorum modi
+          perferendis officiis. Quibusdam commodi quis similique est. Laudantium
+          placeat, distinctio, provident debitis explicabo voluptas dolores
+          omnis soluta minus culpa, quae autem nihil! In repellat velit
+          molestiae, qui a facere odit voluptatem aliquid aut officia numquam
+          voluptates. Numquam impedit expedita nemo corporis excepturi, ut
+          officiis dolorem velit aspernatur voluptate delectus iste facilis at
+          ratione. labore porro ipsa placeat minus nostrum. Provident, saepe
+          libero dolorum modi perferendis officiis. Quibusdam commodi quis
+          similique est. Laudantium placeat, distinctio, provident debitis
+          explicabo voluptas dolores omnis soluta minus culpa, quae autem nihil!
+          In repellat velit molestiae, qui a facere odit voluptatem aliquid aut
+          officia numquam voluptates. Numquam impedit expedita nemo corporis
+          excepturi, ut officiis dolorem velit aspernatur voluptate delectus
+          iste facilis at ratione. labore porro ipsa placeat minus nostrum.
+          Provident, saepe libero dolorum modi perferendis officiis. Quibusdam
+          commodi quis similique est. Laudantium placeat, distinctio, provident
+          debitis explicabo voluptas dolores omnis soluta minus culpa, quae
+          autem nihil! In repellat velit molestiae, qui a facere odit voluptatem
+          aliquid aut officia numquam voluptates. Numquam impedit expedita nemo
+          corporis excepturi, ut officiis dolorem velit aspernatur voluptate
+          delectus iste facilis at ratione. labore porro ipsa placeat minus
+          nostrum. Provident, saepe libero dolorum modi perferendis officiis.
+          Quibusdam commodi quis similique est. Laudantium placeat, distinctio,
+          provident debitis explicabo voluptas dolores omnis soluta minus culpa,
+          quae autem nihil! In repellat velit molestiae, qui a facere odit
+          voluptatem aliquid aut officia numquam voluptates. Numquam impedit
+          expedita nemo corporis excepturi, ut officiis dolorem velit aspernatur
+          voluptate delectus iste facilis at ratione. labore porro ipsa placeat
+          minus nostrum. Provident, saepe libero dolorum modi perferendis
+          officiis. Quibusdam commodi quis similique est. Laudantium placeat,
+          distinctio, provident debitis explicabo voluptas dolores omnis soluta
+          minus culpa, quae autem nihil! In repellat velit molestiae, qui a
+          facere odit voluptatem aliquid aut officia numquam voluptates. Numquam
+          impedit expedita nemo corporis excepturi, ut officiis dolorem velit
+          aspernatur voluptate delectus iste facilis at ratione.
+        </p>
+      </main>
     </>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,14 +3,71 @@ import '../styles/Navbar/Navbar.css';
 
 const Navbar = () => {
   return (
-    <div>
-      <nav>
-        <a href="#Home">Home</a>
-        <a href="#About">About</a>
-        <a href="#Projects">Projects</a>
-        <a href="#Contact">Contact</a>
-      </nav>
-    </div>
+    <nav className="nav">
+      <ul className="nav-list">
+        <li className="logo nav-item">
+          <a href="https://github.com/SirDev97" className="nav-link">
+            <span className="link-text logo-text">Alkin Maystorov</span>
+            <svg
+              aria-hidden="true"
+              focusable="false"
+              dataprefix="fad"
+              dataicon="angle-double-right"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 448 512"
+              className="svg-inline--fa fa-angle-double-right fa-w-14 fa-5x">
+              <g className="fa-group">
+                <path
+                  fill="currentColor"
+                  d="M224 273L88.37 409a23.78 23.78 0 0 1-33.8 0L32 386.36a23.94 23.94 0 0 1 0-33.89l96.13-96.37L32 159.73a23.94 23.94 0 0 1 0-33.89l22.44-22.79a23.78 23.78 0 0 1 33.8 0L223.88 239a23.94 23.94 0 0 1 .1 34z"
+                  className="fa-secondary"></path>
+                <path
+                  fill="currentColor"
+                  d="M415.89 273L280.34 409a23.77 23.77 0 0 1-33.79 0L224 386.26a23.94 23.94 0 0 1 0-33.89L320.11 256l-96-96.47a23.94 23.94 0 0 1 0-33.89l22.52-22.59a23.77 23.77 0 0 1 33.79 0L416 239a24 24 0 0 1-.11 34z"
+                  className="fa-primary"></path>
+              </g>
+            </svg>
+          </a>
+        </li>
+
+        <li className="nav-item">
+          <a href="#Home" className="nav-link">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+              <path d="M575.8 255.5C575.8 273.5 560.8 287.6 543.8 287.6H511.8L512.5 447.7C512.5 450.5 512.3 453.1 512 455.8V472C512 494.1 494.1 512 472 512H456C454.9 512 453.8 511.1 452.7 511.9C451.3 511.1 449.9 512 448.5 512H392C369.9 512 352 494.1 352 472V384C352 366.3 337.7 352 320 352H256C238.3 352 224 366.3 224 384V472C224 494.1 206.1 512 184 512H128.1C126.6 512 125.1 511.9 123.6 511.8C122.4 511.9 121.2 512 120 512H104C81.91 512 64 494.1 64 472V360C64 359.1 64.03 358.1 64.09 357.2V287.6H32.05C14.02 287.6 0 273.5 0 255.5C0 246.5 3.004 238.5 10.01 231.5L266.4 8.016C273.4 1.002 281.4 0 288.4 0C295.4 0 303.4 2.004 309.5 7.014L564.8 231.5C572.8 238.5 576.9 246.5 575.8 255.5L575.8 255.5z" />
+            </svg>
+            <span className="link-text">Home</span>
+          </a>
+        </li>
+
+        <li className="nav-item">
+          <a href="#About" className="nav-link">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+              <path d="M512 32H64C28.65 32 0 60.65 0 96v320c0 35.35 28.65 64 64 64h448c35.35 0 64-28.65 64-64V96C576 60.65 547.3 32 512 32zM176 128c35.35 0 64 28.65 64 64s-28.65 64-64 64s-64-28.65-64-64S140.7 128 176 128zM272 384h-192C71.16 384 64 376.8 64 368C64 323.8 99.82 288 144 288h64c44.18 0 80 35.82 80 80C288 376.8 280.8 384 272 384zM496 320h-128C359.2 320 352 312.8 352 304S359.2 288 368 288h128C504.8 288 512 295.2 512 304S504.8 320 496 320zM496 256h-128C359.2 256 352 248.8 352 240S359.2 224 368 224h128C504.8 224 512 231.2 512 240S504.8 256 496 256zM496 192h-128C359.2 192 352 184.8 352 176S359.2 160 368 160h128C504.8 160 512 167.2 512 176S504.8 192 496 192z" />
+            </svg>
+            <span className="link-text">About</span>
+          </a>
+        </li>
+
+        <li className="nav-item">
+          <a href="#Projects" className="nav-link">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+              <path d="M0 80C0 53.49 21.49 32 48 32H144C170.5 32 192 53.49 192 80V96H384V80C384 53.49 405.5 32 432 32H528C554.5 32 576 53.49 576 80V176C576 202.5 554.5 224 528 224H432C405.5 224 384 202.5 384 176V160H192V176C192 177.7 191.9 179.4 191.7 180.1L272 288H368C394.5 288 416 309.5 416 336V432C416 458.5 394.5 480 368 480H272C245.5 480 224 458.5 224 432V336C224 334.3 224.1 332.6 224.3 331L144 224H48C21.49 224 0 202.5 0 176V80z" />
+            </svg>
+            <span className="link-text">Projects</span>
+          </a>
+        </li>
+
+        <li className="nav-item">
+          <a href="#Contact" className="nav-link">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+              <path d="M384 0H96C60.65 0 32 28.65 32 64v384c0 35.35 28.65 64 64 64h288c35.35 0 64-28.65 64-64V64C448 28.65 419.3 0 384 0zM240 128c35.35 0 64 28.65 64 64s-28.65 64-64 64c-35.34 0-64-28.65-64-64S204.7 128 240 128zM336 384h-192C135.2 384 128 376.8 128 368C128 323.8 163.8 288 208 288h64c44.18 0 80 35.82 80 80C352 376.8 344.8 384 336 384zM496 64H480v96h16C504.8 160 512 152.8 512 144v-64C512 71.16 504.8 64 496 64zM496 192H480v96h16C504.8 288 512 280.8 512 272v-64C512 199.2 504.8 192 496 192zM496 320H480v96h16c8.836 0 16-7.164 16-16v-64C512 327.2 504.8 320 496 320z" />
+            </svg>
+            <span className="link-text">Contact</span>
+          </a>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
