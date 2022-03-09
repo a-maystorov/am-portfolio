f15432d6c8b23eb2a8eec961f01d6ab4abaf66ab
Alkin Maystorov <alkin971@gmail.com>
2022-03-09 14:29:25 +0100

    Initial setup.

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../styles/Navbar/Navbar.css';
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav>
+        <a href="#Home">Home</a>
+        <a href="#About">About</a>
+        <a href="#Projects">Projects</a>
+        <a href="#Contact">Contact</a>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
