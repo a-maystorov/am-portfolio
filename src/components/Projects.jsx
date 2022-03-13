314fbfa65653f485462bb83de140a71eef85e193
Alkin Maystorov <alkin971@gmail.com>
2022-03-13 14:45:21 +0100

    Add links and build tools to projects.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,8 +8,8 @@ const Projects = () => {
         <h2>Projects</h2>
         <p>
           In this section, you will find some of the personal projects that I
-          created with information about the project and the technologies used
-          to build them
+          created with a preview image for the project, links to the repository
+          and live website, along with the tools used to build them
         </p>
       </header>
       <ProjectsCarousel />
