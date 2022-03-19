78552bac80e6f4e9fce845d81b1d38d4554ea6c5
Alkin Maystorov <alkin971@gmail.com>
2022-03-19 14:59:50 +0100

    Finish Footer component.

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import Navbar from './components/Navbar';
 import Header from './components/Header';
 import About from './components/About';
 import Projects from './components/Projects';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 // Styles
 import './App.css';
-import Contact from './components/Contact';
 
 const App = () => {
   return (
@@ -21,6 +22,7 @@ const App = () => {
         <About />
         <Projects />
         <Contact />
+        <Footer />
       </main>
     </>
   );
